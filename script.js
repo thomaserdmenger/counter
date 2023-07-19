ef06dd8990539b76c78fb3b6cd4d0aa45285eae5
Thomas Erdmenger <thomas.erdmenger@icloud.com>
2023-07-19 16:59:48 +0200

    Rotate reload after every click

---
+++ script.js
@@ -0,0 +1,20 @@
+// Get DOM elements
+const counterEl = document.querySelector('.display__number');
+const reloadEl = document.querySelector('.button--reload');
+const buttonChangeArr = document.querySelectorAll('.button--change');
+
+// Rotate reload button
+const rotateReload = () => {
+  reloadEl.classList.add('display__reload--animation');
+};
+
+const resetReload = () => {
+  reloadEl.classList.remove('display__reload--animation');
+};
+
+const delayResetReload = () => {
+  setTimeout(resetReload, 1500);
+};
+
+reloadEl.addEventListener('mousedown', rotateReload);
+reloadEl.addEventListener('mouseup', delayResetReload);
